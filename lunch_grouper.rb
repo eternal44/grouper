d21e12f24d6fee53519d10037d7b97433dff7334
James Youn <jamesyoun710@gmail.com>
2017-12-17 21:35:00 -0800

    Adds call signature

--- lunch_grouper.rb
+++ lunch_grouper.rb
@@ -1,21 +1,29 @@
 require 'optparse'
+require './group_generator.rb'
 
-options = {}
+def file_storage_path
+  './grouper.txt'
+end
+
+def participants
+  File.readlines(file_storage_path)
+end
 
 OptionParser.new do |opts|
-  opts.banner = "Usage: example.rb [options]"
+  opts.banner = "Usage: ruby lunch_grouper [options]"
 
-  opts.on('-a', '--add  x,y,z', Array, 'Adds new participants') do |v|
-    options[:new_people] = v
-  end
-
-  opts.on('-l', '--list', 'Lists participants') do
-    p 'List of people'
+  opts.on('-a', '--add  x,y,z', Array, 'Adds new participants') do |new_lunchers|
+    GroupGenerator.add_new_lunchers(file_storage_path, new_lunchers)
   end
 
   opts.on('-g', '--generate', 'Generates random groups of 3-5 participants') do
+    GroupGenerator.generate_groups(participants)
+  end
+
+  opts.on('-l', '--list', 'Lists participants') do
+    participants.readlines.each do |participant|
+      p participant.strip
+    end
   end
 end.parse!
 
-if __FILE__ == $PROGRAM_NAME
-end
